b7810da7b6c80ad30537ece5ad027f8f52075281
Clarosabel2 <clarosabel2@gmail.com>
2023-09-05 12:07:20 -0300

    ASDASD

--- holamundo.py
+++ holamundo.py
@@ -1,3 +1,14 @@
 print("Hola mundo")
 print("")
+WindowsErrorRE
+RecursionErrorFG
+DFG
+DF
+GeneratorExitDFG
+DeprecationWarningGD
+FloatingPointErrorDF
+GeneratorExitDFG
+DeprecationWarningGFD
+GFD
+GeneratorExitG
       
