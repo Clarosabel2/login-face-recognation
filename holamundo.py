4b92f793d4883dd91bafdebfcb0c3ef6be25da95
DavidB954 <david_o_bellido@hotmail.com>
2023-09-05 12:07:40 -0300

    3
    3

--- holamundo.py
+++ holamundo.py
@@ -1,3 +1,13 @@
 print("Hola mundo")
 print("")
-      
+Downloading imutils-0.5.4.tar.gz (17 kB)
+  Installing build dependencies ... done
+  Getting requirements to build wheel ... done
+  Preparing metadata (pyproject.toml) ... done
+Building wheels for collected packages: imutils
+  Building wheel for imutils (pyproject.toml) ... done
+  Created wheel for imutils: filename=imutils-0.5.4-py3-none-any.whl size=25855 sha256=bdfd89e211b0a21bcad248ab1b1d2c7fe367e0b1e4b6180259ddee6f7878d57d
+  Stored in directory: c:\users\david bellido\appdata\local\pip\cache\wheels\31\d0\2c\87ce38f6052879e5b7b18f0f8b4a10ad2a9d210e908d449f16
+Successfully built imutils
+Installing collected packages: imutils
+Successfully installed imutils-0.5.4
